bc67027772ed6723d5247fbab993041b3d22ebaf
joash <joeljoash001@gmail.com>
2021-06-22 10:23:18 +0300

    changes

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".design").click(function() {
+        $(".design").toggle();
+        $(".design-hidden") .toggle();
+    }
+    $(".design").click(function() {
+        $(".design").toggle();
+        $(".design-hidden") .toggle();
+    }
+);
+$(".development").click(function() {
+    $(".development-showing").toggle();
+    $(".development-hidden").toggle();
+})
+$(".product").click(function() {
+    $(".product-showing").toggle();
+    $(".product-hidden").toggle();
+})});
