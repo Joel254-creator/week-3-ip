b1fa52c549c9bcbb9a6db90bd6a88ed30555942a
Joel254-creator <85164035+Joel254-creator@users.noreply.github.com>
2021-06-22 16:27:47 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(".design").click(function() {
+        $(".design").toggle();
+        $(".design-hidden") .toggle();
+    }
+    $(".design").click(function() {
+        $(".design").toggle();
+        $(".design-hidden") .toggle();
+    }
+);
+$(".development").click(function() {
+    $(".development-showing").toggle();
+    $(".development-hidden").toggle();
+})
+$(".product").click(function() {
+    $(".product-showing").toggle();
+    $(".product-hidden").toggle();
+})});.mouse-click {
+    animation: bounce 3s infinite;
+}
+  @keyframes bounce {
+    0%,
+    20%,
+    50#mouse_click{
+      animation: bounce 2s infinite;  
+      padding-top: 20px;  
+      padding-bottom:20px;%,
+    80%,
+    100% {
+        transform: translateY(0);
+}
+    40% {
+        transform: translateY(-55px);
+    }
+    60% {
+        transform: translateY(-45px);
+}
+
