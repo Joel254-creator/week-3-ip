1fe03be6287177b9134135385d95ac08098b3666
joash <joeljoash001@gmail.com>
2021-06-23 08:58:48 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -15,4 +15,24 @@ $(".development").click(function() {
 $(".product").click(function() {
     $(".product-showing").toggle();
     $(".product-hidden").toggle();
-})});
+})});.mouse-click {
+    animation: bounce 3s infinite;
+}
+  @keyframes bounce {
+    0%,
+    20%,
+    50#mouse_click{
+      animation: bounce 2s infinite;  
+      padding-top: 20px;  
+      padding-bottom:20px;%,
+    80%,
+    100% {
+        transform: translateY(0);
+}
+    40% {
+        transform: translateY(-55px);
+    }
+    60% {
+        transform: translateY(-45px);
+}
+
